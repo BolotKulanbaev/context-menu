58996d1fac1202ee96f8a15baf2db5c808a435f4
Пользователь <kulanbaev.bolot11gmail.com>
2022-06-27 16:47:24 +0600

    contacts

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,6 +2,7 @@ import { Button } from "@mui/material";
 import React, { useContext, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { todoContext } from "../../contexts/todoContext";
+import "./Home.css";
 
 const List = () => {
   const { getTodos, todos, deleteTodo } = useContext(todoContext);
@@ -11,26 +12,24 @@ const List = () => {
   }, []);
   console.log(todos);
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        justifyContent: "center",
-      }}>
+    <div className="home">
       {todos.map(item => (
         <div key={item.id}>
-          <div>{item.name}</div>
+          <div
+            className="home-names"
+            onClick={() => navigate(`/edit/${item.id}`)}>
+            {item.name}
+          </div>
           {/* <div>{item.phone}</div>
           <div>{item.email}</div> */}
 
           {/* <button onClick={() => deleteTodo(item.id)}>Delete</button> */}
           {/* <button onClick={() => navigate(`/edit/${item.id}`)}>Details</button> */}
-          <Button
+          {/* <Button 
             onClick={() => navigate(`/edit/${item.id}`)}
             variant="contained">
             Details
-          </Button>
+          </Button> */}
         </div>
       ))}
     </div>
--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,6 +1,9 @@
 import React, { useContext, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { todoContext } from "../../contexts/todoContext";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import "./List.css";
 
 const List = () => {
   const { getTodos, todos, deleteTodo } = useContext(todoContext);
@@ -10,21 +13,25 @@ const List = () => {
   }, []);
   console.log(todos);
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        justifyContent: "center",
-      }}>
+    <div className="list">
       {todos.map(item => (
-        <div key={item.id}>
-          <div>{item.name}</div>
-          <div>{item.phone}</div>
-          <div>{item.email}</div>
+        <div className="list-card" key={item.id}>
+          <Typography gutterBottom variant="h5" component="div">
+            {item.name}
+          </Typography>
+          <Typography gutterBottom variant="h5" component="div">
+            {item.phone}
+          </Typography>
+          <Typography gutterBottom variant="h5" component="div">
+            {item.email}
+          </Typography>
 
-          <button onClick={() => deleteTodo(item.id)}>Delete</button>
-          <button onClick={() => navigate(`/edit/${item.id}`)}>Edit</button>
+          <Button onClick={() => deleteTodo(item.id)} size="small">
+            Delete
+          </Button>
+          <Button onClick={() => navigate(`/edit/${item.id}`)} size="small">
+            Edit
+          </Button>
         </div>
       ))}
     </div>
